17f695b3c836223d39188bbc291d959902f6d9fd
Harry Poggers <mfa.g4e@gmail.com>
2024-07-30 23:30:46 +0800

    Streamlit still no working
    - something to do with sc.fit_transform() and sc.transform()

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ sc = StandardScaler()
 
 # Define the input fields for the features
 def user_input_data():
-    sample_code_number = st.text_input('Sample Code Number', value='0')    
-    clump_thickness = st.number_input('Clump Thickness', min_value=1, max_value=10, value=5)
-    uniformity_of_cell_size = st.number_input('Uniformity of Cell Size', min_value=1, max_value=10, value=5)
-    uniformity_of_cell_shape = st.number_input('Uniformity of Cell Shape', min_value=1, max_value=10, value=5)
-    marginal_adhesion = st.number_input('Marginal Adhesion', min_value=1, max_value=10, value=5)
-    single_epithelial_cell_size = st.number_input('Single Epithelial Cell Size', min_value=1, max_value=10, value=5)
-    bare_nuclei = st.number_input('Bare Nuclei', min_value=1, max_value=10, value=5)
-    bland_chromatin = st.number_input('Bland Chromatin', min_value=1, max_value=10, value=5)
-    normal_nucleoli = st.number_input('Normal Nucleoli', min_value=1, max_value=10, value=5)
-    mitoses = st.number_input('Mitoses', min_value=1, max_value=10, value=5)
+    sample_code_number = st.text_input('Sample Code Number', value='1018561')    
+    clump_thickness = st.number_input('Clump Thickness', min_value=1, max_value=10, value=2)
+    uniformity_of_cell_size = st.number_input('Uniformity of Cell Size', min_value=1, max_value=10, value=1)
+    uniformity_of_cell_shape = st.number_input('Uniformity of Cell Shape', min_value=1, max_value=10, value=2)
+    marginal_adhesion = st.number_input('Marginal Adhesion', min_value=1, max_value=10, value=1)
+    single_epithelial_cell_size = st.number_input('Single Epithelial Cell Size', min_value=1, max_value=10, value=2)
+    bare_nuclei = st.number_input('Bare Nuclei', min_value=1, max_value=10, value=1)
+    bland_chromatin = st.number_input('Bland Chromatin', min_value=1, max_value=10, value=3)
+    normal_nucleoli = st.number_input('Normal Nucleoli', min_value=1, max_value=10, value=1)
+    mitoses = st.number_input('Mitoses', min_value=1, max_value=10, value=2)
     
     data = {
         'Sample Code Number': sample_code_number,
@@ -51,15 +51,13 @@ st.write(pd.DataFrame(data, index=[0]))
 if st.button('Predict'):
     # Predict the class using the model
     data_array = []
-    for key, value in data:
+    for key, value in data.items():
         data_array.append(int(value))
     
+    sc.transform(data_array)
     prediction = model.predict(sc.transform([data_array]))
 
     if prediction[0] == 1: 
         st.success('Class 4 (Malignant)')
     else: 
         st.error('Class 2 (Benign)') 
-
-
-
