980c184dfbed2c496377c4b876109523efcfe3d2
Harry Poggers <mfa.g4e@gmail.com>
2024-07-30 22:52:22 +0800

    Fixed streamlit problem

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ from sklearn.preprocessing import StandardScaler
 # Load the trained model with the absolute path
 model_path = 'decision_tree_model.pkl'
 model = joblib.load(model_path)
+sc = StandardScaler()
 
 # Define the input fields for the features
-def user_input_features():
+def user_input_data():
     sample_code_number = st.text_input('Sample Code Number', value='0')    
     clump_thickness = st.number_input('Clump Thickness', min_value=1, max_value=10, value=5)
     uniformity_of_cell_size = st.number_input('Uniformity of Cell Size', min_value=1, max_value=10, value=5)
@@ -22,37 +23,38 @@ def user_input_features():
     normal_nucleoli = st.number_input('Normal Nucleoli', min_value=1, max_value=10, value=5)
     mitoses = st.number_input('Mitoses', min_value=1, max_value=10, value=5)
     
-    data = [
-        int(sample_code_number),
-        int(clump_thickness),
-        int(uniformity_of_cell_size),
-        int(uniformity_of_cell_shape),
-        int(marginal_adhesion),
-        int(single_epithelial_cell_size),
-        int(bare_nuclei),
-        int(bland_chromatin),
-        int(normal_nucleoli),
-        int(mitoses)
-    ]
-
-    sc = StandardScaler()
-    return sc.transform([data])
-    
+    data = {
+        'Sample Code Number': sample_code_number,
+        'Clump Thickness': clump_thickness,
+        'Uniformity of Cell Size': uniformity_of_cell_size,
+        'Uniformity of Cell Shape': uniformity_of_cell_shape,
+        'Marginal Adhesion': marginal_adhesion,
+        'Single Epithelial Cell Size': single_epithelial_cell_size,
+        'Bare Nuclei': bare_nuclei,
+        'Bland Chromatin': bland_chromatin,
+        'Normal Nucleoli': normal_nucleoli,
+        'Mitoses': mitoses
+    }
+    return data    
 
 # Title of the app
 st.title('Cancer Cell Prediction App')
 
 # Get user input
-input_df = user_input_features()
+data = user_input_data()
 
 # Display user input
 st.subheader('User Input features')
-st.write(input_df)
+st.write(pd.DataFrame(data, index=[0]))
 
 # Add a button to make the prediction
 if st.button('Predict'):
     # Predict the class using the model
-    prediction = model.predict(input_df)
+    data_array = []
+    for key, value in data:
+        data_array.append(int(value))
+    
+    prediction = model.predict(sc.transform([data_array]))
 
     if prediction[0] == 1: 
         st.success('Class 4 (Malignant)')
